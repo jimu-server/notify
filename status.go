85461b2393321cdcb449dbe01522bdd3c0f064fa
awen <zhiwen_der@qq.com>
2024-06-08 18:06:21 +0800

    update

--- status.go
+++ status.go
@@ -1,11 +1,17 @@
 package notify
 
 const (
+	// UnRead 未读
 	UnRead = 0
-	Read   = 1
+	// Read 已读
+	Read = 1
 
-	Info    = 1
+	// Info 一版消息
+	Info = 1
+	// Success 操作成功消息
 	Success = 2
-	Warn    = 3
-	Error   = 4
+	// Warn 警告消息
+	Warn = 3
+	// Error 错误消息
+	Error = 4
 )
